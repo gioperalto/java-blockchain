d1170afe5065c1b949959a1b968ceb5612a50938
gioperalto <gyoperalto@gmail.com>
2019-12-27 15:43:53 -0500

    defined block class

---
+++ src/Block.java
@@ -0,0 +1,40 @@
+/**
+ * Created by gioperalto on 12/27/19.
+ */
+import java.util.Date;
+
+import java.util.Date;
+
+public class Block {
+
+    public String hash, previousHash;
+    private String data;
+    private long timeStamp;
+    private int nonce;
+
+    public Block(String data,String previousHash ) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+
+        this.hash = calculateHash();
+    }
+
+    public String calculateHash() {
+        return StringUtil.applySha256(
+                previousHash +
+                        Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
+                        data
+        );
+    }
+
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+        while(!hash.substring( 0, difficulty).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
+    }
+}
