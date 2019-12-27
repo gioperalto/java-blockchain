756d7bbf7eb24dd774be9439603c41cece019825
gioperalto <gyoperalto@gmail.com>
2019-12-27 16:08:50 -0500

    Made input dynamic so users can pass in amount of blocks and difficulty

--- src/YavaChain.java
+++ src/YavaChain.java
@@ -1,33 +1,42 @@
 /**
  * Created by gioperalto on 12/27/19.
  */
+import java.util.Scanner;
 import java.util.ArrayList;
 import com.google.gson.GsonBuilder;
 
 public class YavaChain {
 
     public static ArrayList<Block> blockchain = new ArrayList<>();
-    public static int difficulty = 5;
+    public static int difficulty = 1;
 
     public static void main(String[] args) {
-        //add our blocks to the blockchain ArrayList:
+        Scanner in = new Scanner(System.in);
 
-        blockchain.add(new Block("Hi im the first block", "0"));
-        System.out.println("Trying to Mine block 1... ");
-        blockchain.get(0).mineBlock(difficulty);
+        System.out.println("Welcome to YavaChain\n--------------");
+        System.out.println("How many blocks would you like to mine? (Default 3)");
+        int blocks = in.nextInt();
 
-        blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).hash));
-        System.out.println("Trying to Mine block 2... ");
-        blockchain.get(1).mineBlock(difficulty);
+        System.out.println("Choose your difficulty (Default 1)... Warning: Anything beyond 6 could take a while");
+        difficulty = in.nextInt();
 
-        blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).hash));
-        System.out.println("Trying to Mine block 3... ");
-        blockchain.get(2).mineBlock(difficulty);
+        for(int i = 0; i < blocks; i++) {
+            if(i == 0) {
+                blockchain.add(new Block(String.format("Block %d", i+1), "0"));
+            } else {
+                blockchain.add(
+                    new Block(String.format("Block %d", i+1),
+                    blockchain.get(blockchain.size()-1).hash)
+                );
+            }
+            System.out.printf("\nTrying to Mine block %d... ", i+1);
+            blockchain.get(i).mineBlock(difficulty);
+        }
 
         System.out.println("\nBlockchain is Valid: " + isChainValid());
 
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
-        System.out.println("\nThe block chain: ");
+        System.out.println("\nFull YavaChain: ");
         System.out.println(blockchainJson);
     }
 
@@ -38,19 +47,17 @@ public class YavaChain {
         for(int i=1; i < blockchain.size(); i++) {
             currentBlock = blockchain.get(i);
             previousBlock = blockchain.get(i-1);
-            //compare registered hash and calculated hash:
+
             if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
                 System.out.println("Current Hashes not equal");
                 return false;
             }
 
-            //compare previous hash and registered previous hash
             if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
                 System.out.println("Previous Hashes not equal");
                 return false;
             }
 
-            //check if hash is solved
             if(!currentBlock.hash.substring( 0, difficulty).equals(hashTarget)) {
                 System.out.println("This block hasn't been mined");
                 return false;
